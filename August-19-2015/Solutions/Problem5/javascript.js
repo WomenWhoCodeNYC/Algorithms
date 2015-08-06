f5371f5b8e9f386737ec776ff647db3906292ecb
Karen Mao <karen.mao707@gmail.com>
2015-08-05 23:01:48 -0400

    solution5 added

---
+++ August-19-2015/Solutions/Problem5/javascript.js
@@ -0,0 +1,22 @@
+function permutation(inputStr){
+  var results = [];
+ 
+  var inputArr = inputStr.split('');
+  function permute(arr, memo) {
+    var cur, memo = memo || [];
+
+    for (var i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+     var permutation = []
+     results.forEach(function(perm){permutation.push(perm.join(""))});
+    return permutation;
+  }
+
+  return permute(inputArr);
+}
