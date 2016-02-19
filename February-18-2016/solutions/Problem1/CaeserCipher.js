74323599872563c661e241b6779bc1de978adf53
Sneha Inguva <snehainguva@Snehas-MacBook-Pro.local>
2016-02-18 21:06:54 -0500

    caeser cipher code yay!!!! :) :)

---
+++ February-18-2016/solutions/Problem1/CaeserCipher.js
@@ -0,0 +1,22 @@
+function getNewCode(code,key){
+    var result;
+	if (code >= 97 && code <= 122){
+        result = ((code + key - 96) % 26) + 96; 
+    }else if(code >= 65 && code <= 90){
+        result = ((code + key - 64) % 26) + 64;   
+    }else{
+        result = code;
+    }
+    return result;
+}
+
+function cencrypt(s, key){
+    var result = "";
+	for (i = 0 ; i < s.length; i++){
+	    var code = s.charCodeAt(i);
+	    result += String.fromCharCode(getNewCode(code,key));
+    }
+    return result;
+}
+
+console.log(cencrypt("zFabc",3));
