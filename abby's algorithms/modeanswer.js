9d516bf649e581a1ae581fe21cb708e5dcc270dd
abby-garcia <acg361@gmail.com>
2016-03-11 16:46:25 -0500

    mode q and a in js

---
+++ abby's algorithms/modeanswer.js
@@ -0,0 +1,29 @@
+function SimpleMode(arr) {
+
+    var map = {};
+    var mode = 0;
+    var highestFreq = 0;
+
+    for (i = 0; i < arr.length; i++) {
+        if (map[arr[i]]) {
+            map[arr[i]]++;
+        } else {
+            map[arr[i]] = 1;
+        }
+    }
+    
+    for (i = 0; i < arr.length; i++) {
+        if (map[arr[i]] > highestFreq) {
+            highestFreq = map[arr[i]];
+            mode = arr[i];
+        }
+    }
+    
+    if (highestFreq !== 1) {
+        return mode;
+    } else {
+        return -1;
+    }
+    
+    return mode;
+}
---
+++ abby's algorithms/modequestion.js
@@ -0,0 +1,14 @@
+Using the JavaScript language, have the function SimpleMode(arr) take the array of numbers stored in arr and return the number that appears most frequently (the mode). For example: if arr contains [10, 4, 5, 2, 4] the output should be 4. If there is more than one mode return the one that appeared in the array first (ie. [5, 10, 10, 6, 5] should return 5 because it appeared first). If there is no mode return -1. The array will not be empty. 
+
+
+
+
+function SimpleMode(arr) { 
+
+  // code goes here  
+  return arr; 
+         
+}
+   
+// keep this function call here 
+SimpleMode(readline());          
