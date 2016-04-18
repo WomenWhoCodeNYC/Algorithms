6aa8ffcc304c3492ad6c4fcf5456d714b81329f1
Sarah Zinger <sarah.a.zinger@gmail.com>
2016-04-18 10:32:44 -0400

    Add April's Problem file. Move CompareTwoStrings out of nested file.

--- challenges/CompareTwoStrings/solutions/compareTwoStrings.cpp
+++ challenges/CompareTwoStrings/solutions/compareTwoStrings.cpp
@@ -1,14 +1,15 @@
+// submitted by https://github.com/ayunav
 // C++ program to compare two strings represented as linked lists
 #include<bits/stdc++.h>
 using namespace std;
-  
-//  Linked list Node is defined as 
-struct Node  
+
+//  Linked list Node is defined as
+struct Node
 {
     char c;
     struct Node *next;
 };
-  
+
 // Function to create newNode in a linkedlist
 Node* newNode(char c)
 {
@@ -17,28 +18,28 @@ Node* newNode(char c)
     temp->next = NULL;
     return temp;
 };
- 
-int compare(Node *list1, Node *list2) 
-{    
-    // Traverse both lists. Stop when either end of a linked 
+
+int compare(Node *list1, Node *list2)
+{
+    // Traverse both lists. Stop when either end of a linked
     // list is reached or current characters don't match
-    while (list1 && list2 && list1->c == list2->c) 
-    {         
+    while (list1 && list2 && list1->c == list2->c)
+    {
         list1 = list1->next;
         list2 = list2->next;
     }
-     
+
     //  If both lists are not empty, compare mismatching
     //  characters
-    if (list1 && list2) 
+    if (list1 && list2)
         return (list1->c > list2->c)? 1: -1;
- 
+
     // If either of the two lists has reached end
     if (list1 && !list2) return 1;
     if (list2 && !list1) return -1;
-     
-    // If none of the above conditions is true, both 
-    // lists have reached end 
+
+    // If none of the above conditions is true, both
+    // lists have reached end
     return 0;
 }
 
@@ -51,16 +52,16 @@ int main()
     list1->next->next->next = newNode('k');
     list1->next->next->next->next = newNode('s');
     list1->next->next->next->next->next = newNode('b');
- 
+
     Node *list2 = newNode('g');
     list2->next = newNode('e');
     list2->next->next = newNode('e');
     list2->next->next->next = newNode('k');
     list2->next->next->next->next = newNode('s');
     list2->next->next->next->next->next = newNode('a');
- 
+
     cout << compare(list1, list2);
-  
+
     return 0;
 }
 
