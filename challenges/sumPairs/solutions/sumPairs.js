e31c7faba422fec6a914e3a9644b5a61d7b1fe8e
freda nada <fredasnada@gmail.com>
2017-03-16 15:54:14 -0400

    simplify

--- challenges/sumPairs/solutions/sumPairs.js
+++ challenges/sumPairs/solutions/sumPairs.js
@@ -1,10 +1,7 @@
 const sumPairs = (arr, tgt) => {
   const hash = {}
   for (let i=0, len=arr.length; i<len; i++) {
-    if (hash[tgt-arr[i]]) {
-      const idx = arr.indexOf(tgt-arr[i])
-      return [arr[idx], arr[i]]
-    }
+    if (hash[tgt-arr[i]]) return [tgt-arr[i], arr[i]]
     hash[arr[i]] = true
   }
   return false
