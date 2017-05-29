2a633cc0132dc6ed772182039dc449518d19d18f
dushyanthi pieris <dushi27@gmail.com>
2017-05-29 13:16:06 -0400

    removed credit card question

--- challenges/creditCardMask/solutions/cc_mask.rb
+++
@@ -1,9 +0,0 @@
-def maskify(str: raise)
-	return str if str.size <= 4
-	"#{'#' * (str.size - 4)}#{str[-4..-1]}" 
-end
-
-puts maskify(str:"4556364607935616") == "############5616"
-puts maskify(str: "1") ==                "1"
-puts maskify(str: "") ==                 ""
-puts maskify(str: "Skippy") == "##ippy"
